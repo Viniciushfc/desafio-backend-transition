c7b60e97efd9fac945e16ea824e9121a392e1ade
ViniciusHFC <vinicius.henry.fc@gmail.com>
2023-12-27 21:19:28 -0300

    First Commit

---
+++ src/test/java/com/desafio/transition/TransitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.transition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
