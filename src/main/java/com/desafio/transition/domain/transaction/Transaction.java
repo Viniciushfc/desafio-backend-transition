e708588f9e68bb0a52f13e89b8f74dc7bbe9c5dd
ViniciusHFC <vinicius.henry.fc@gmail.com>
2023-12-28 15:18:07 -0300

    Created Transaction

---
+++ src/main/java/com/desafio/transition/domain/transaction/Transaction.java
@@ -0,0 +1,33 @@
+package com.desafio.transition.domain.transaction;
+
+import com.desafio.transition.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @JoinColumn(name = "receiver_id")
+    @ManyToOne
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
